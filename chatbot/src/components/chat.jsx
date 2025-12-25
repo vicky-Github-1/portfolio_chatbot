4baa9766ad44e87ba22ee6f70bc90126f684f4b7
shubham <shubhamgupta23844@gmail.com>
2025-12-25 11:47:21 +0530

    Use env variable for backend API

--- chatbot/src/components/chat.jsx
+++ chatbot/src/components/chat.jsx
@@ -25,7 +25,7 @@ export default function WhatsAppChat() {
     setInput("");
 
     try {
-      const res = await fetch("http://localhost:5000/chat", {
+      const res = await fetch(`${import.meta.env.VITE_API_URL}/chat`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ message: input }),
